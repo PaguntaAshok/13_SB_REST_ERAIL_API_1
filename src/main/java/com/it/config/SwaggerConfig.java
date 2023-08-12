b070300418863ba90e8ed04f26794334e2536e94
ASK123PROJECT <paguntaashokreddy@gmail.com>
2023-08-12 23:27:37 +0530

    initial commit

---
+++ src/main/java/com/it/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.it.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	//http://localhost:9092/swagger-ui.html
+	//http://3.110.94.91:9092/swagger-ui.html
+	//http://localhost:9092/v2/api-docs
+	public Docket apiDoc() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.it.rest"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+}
